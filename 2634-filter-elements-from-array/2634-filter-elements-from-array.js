499146989e4a452bdd41a35bdb8ae7bdb167461b
Rajeev Kumar Raja <75115330+rajeevkr07@users.noreply.github.com>
2026-03-10 17:04:35 +0530

    Time: 22 ms (99.57%), Space: 52.8 MB (94.74%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -11,7 +11,7 @@ var filter = function(arr, fn) {
   const res = [];
    
     for(let i=0;i< arr.length; i++) {
-           if(fn(arr[i], Number(i))){
+           if(fn(arr[i], i)){
             res.push(arr[i]);
            }
     }
