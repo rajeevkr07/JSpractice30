70eabd2b15af31f398ae0725873eb04cc8a62c30
Rajeev Kumar Raja <75115330+rajeevkr07@users.noreply.github.com>
2026-03-11 00:02:16 +0530

    Time: 56 ms (7.77%), Space: 56.3 MB (6.24%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+
+    let res = init;
+
+    for(const n of nums){
+        res = fn(res, n)
+    }
+    return res;
+
+    // nums.forEach(() => {
+    //     res = fn(res, n);
+    // })
+    // return res;
+     
+    //  return nums.reduce(fn, init)
+};
