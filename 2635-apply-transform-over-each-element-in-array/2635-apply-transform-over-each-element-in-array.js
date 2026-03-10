db48325cd2705f691c292b292907595dd61a60ba
Rajeev Kumar Raja <75115330+rajeevkr07@users.noreply.github.com>
2026-03-10 16:51:14 +0530

    Time: 37 ms (87.16%), Space: 53.9 MB (42.27%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    const res = []; // new Array(arr.length);
+    const res = []; // new Array(arr.length); dynamic array
 
     for(const i in arr){
        res.push(fn(arr[i], Number(i)))
@@ -12,6 +12,6 @@ var map = function(arr, fn) {
   
    return res;
 };
-
+ // The strategy design pattern  -Open-closed principal
  // return arr.map(fn);
     // map comes from Functional programming no state, no variables
